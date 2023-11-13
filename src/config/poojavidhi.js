60917248b2115c8582391fb6aa81107f01d58a3b
Abhishek Patria <patriaabhishek@gmail.com>
2023-11-13 22:40:54 +0530

    Added Pooja Vidhi

---
+++ src/config/poojavidhi.js
@@ -0,0 +1,109 @@
+export const PoojaVidhiConfig = {
+  items: [
+    {
+      parent:
+        "Satimata Pooja is usually carried out on the day of “Dihadi Ashtami” (Ashtami of Shukla Paksha)",
+    },
+
+    {
+      parent:
+        "Pooja process starts with cleaning of all the Chabutras’ floor using broomstick, mop and a clean cloth. Ensure that the broomstick and mop does NOT touch the Pratima",
+    },
+
+    {
+      parent:
+        "Once the floors and benches of the Chabutras are cleaned, Pratimas of 6 Satimata and 3 Piteshwar are cleaned using clean water and cloth. Ensure that you remove the exiting Mali Pana covering the Pratimas",
+    },
+
+    {
+      parent:
+        "Dihadi Maata is revered at the residence of Shri Om Ji Patria, by offering nariyal, maala, patashe, mithai, red and white cloth prior to starting of the procession. Diya and agarbatti is lit at their residence",
+    },
+
+    {
+      parent:
+        "Pooja procession, comprising of Patria family ladies and children, starts from Uparla Chowk or Patria Chowk in Jahazpur towards Satimata Sthal",
+    },
+
+    {
+      parent:
+        "The head female member of the family organizing the pooja, should wear a saari and walks barefoot in the procession carrying the bhog of Lapsi and Chawal",
+    },
+
+    {
+      parent:
+        "This procession is accompanied with ladies singing Mangal Geet (folk song) on beats of a Nagada (musical instrument played with 2 sticks on a tabla like drum)",
+    },
+
+    {
+      parent:
+        "It has to be noted that Dhol is NOT to be used (strictly banned) and females should not wear blue colored saari/ clothing",
+    },
+
+    {
+      parent:
+        "After the procession reaches the Satimata Sthal, the following pooja is carried out at all the Chabutras of Satimata and Piteshwar, starting with the main Satimata (badi) and the following order given in the image below:",
+
+      children: [
+        {
+          parent:
+            "A coat of sindoor and pure cow milk ghee is applied to every Pratima of Satimata and Piteshwar",
+        },
+        {
+          parent:
+            "This is followed by application of Maali Pana to all the Pratimas of Satimata and Piteshwar. The coat of sindoor and ghee helps in sticking the Mali Pana to the Pratimas",
+        },
+        {
+          parent:
+            "Then the Bahu (daughter in law) of the family offers wheat grains, agarbatti, patashe, gola, mehandi, and maawa mithai to each of the Satimata Pratima",
+        },
+        {
+          parent:
+            "An Saari is offered to the main Satimata. The Saari is placed on the top of the Pratima and then secured by tying a laccha around the Pratima. The Orange Saari is only offered to the main Satimata (badi). This is followed by application of kumkum, akshat (rice), mala, lakh ka chuda, kaajal, bindi and itra (perfume) on every Pratima of the Satimata. ",
+        },
+        {
+          parent:
+            "If the Piteshwar is present in the chabutra, a white cloth is offered to them. This is followed by application of kumkum, akshat (rice), mala, and itra (perfume) on every Pratima of the Piteshwar. ",
+        },
+        {
+          parent:
+            "Then diya and agarbatti are lit, and the gola, patashe, paan, panchamrat, mithai and lapsi chawal bhog is offered to both Satimata and Piteshwar respectively.",
+        },
+        {
+          parent:
+            "Kande are lit and ghee, kapoor, a piece of maave ki mithai, and a small piece of gola which was offered in the previous step, is added to the burning kanda as bhog to the Pratima",
+        },
+      ],
+    },
+
+    {
+      parent:
+        "When the above process is completed at all the Chabutras, the daughter of the family performing the pooja, collects all the Saari at the Pratimas. The white cloth offered for the Piteshwar is left as it is",
+    },
+
+    {
+      parent:
+        "Now everyone gathers together at the Chabutra of the main Satimata (badi) and traditional Mangal Geet are sun",
+    },
+
+    {
+      parent:
+        "The mehandi that was brought individually by ladies is collected in a Paraat (utensil) and water is added to make a paste. Mehandi is applied on the palm of all the ladies. Small amount of this mehandi is also carried by the ladies for other family members who could not attend",
+    },
+
+    {
+      parent:
+        "Golas which were offered at different Chabutras are also collected and crushed in a mortar and pestle. Paan, patashe and maave ki mithai are then added to the same mortal and pestle with crushed golas to make the main prashad. This prashad is offered to all the family members and the adjoining Barah Devra mandi",
+    },
+
+    {
+      parent:
+        "Bhojan prasadi comprising of daal, baati, and churma is prepared on kande/ jagra/ cow dung cakes.",
+    },
+
+    {
+      parent:
+        "After the pooja is over, the Satimata Chabutras are closed to avoid any animal nuisance.",
+    },
+  ],
+};
---
+++ src/pages/poojavidhi.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import Layout from "../components/layout";
+import { PoojaVidhiConfig } from "../config/poojavidhi";
+
+const OrderedList = ({ items }) => {
+  return (
+    <List component="ol" sx={{ listStyle: "decimal", pl: 4 }}>
+      {items.map((item, index) => (
+        <ListItem key={index} sx={{ display: "list-item" }}>
+          <ListItemText primary={item.parent} />
+          {item.children && item.children.length > 0 && (
+            <OrderedList items={item.children} />
+          )}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+const PoojaVidhi = () => {
+  return (
+    <Layout pageTitle="Pooja Vidhi">
+      <Typography gutterBottom variant="h4" component="div" align="center">
+        Pooja Vidhi
+      </Typography>
+      <Typography variant="body2">
+        <OrderedList items={PoojaVidhiConfig.items} />
+      </Typography>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>Pooja Vidhi</title>;
+
+export default PoojaVidhi;
