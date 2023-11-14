f73956bfdf96300bb1fb802670fef23fc84d4d04
Abhishek Patria <patriaabhishek@gmail.com>
2023-11-14 06:07:05 +0530

    Added pooja considerations and updated pooja vidhi alignment

---
+++ src/config/poojaconsiderations.js
@@ -0,0 +1,24 @@
+export const PoojaConsiderations = {
+  items: [
+    {
+      title: "Pooja Conduct",
+      content:
+        "At our temple, there are no specific pandits designated for the Sati Mata Pooja. Every devotee, irrespective of gender, is warmly invited to perform this sacred ritual. It is a testament to the equality and inclusivity inherent in the worship of Satimata Ji.",
+    },
+    {
+      title: "Attire Considerations",
+      content:
+        "While engaging in the Pooja, it is advised that females refrain from adorning blue-colored clothing. This observance is rooted in tradition, ensuring a harmonious and spiritually attuned atmosphere during the worship.",
+    },
+    {
+      title: "Saradhh Timings",
+      content:
+        "The Saradhh of Satimata Ji can be conducted with heightened devotion on the auspicious tithis of Saptmi, Astmi, Dasmi, Dwadsi, and Chaturdashi. These sacred occasions provide a spiritually charged backdrop for connecting with the divine.",
+    },
+    {
+      title: "Bhog Offering",
+      content:
+        "The offering of bhog, particularly the dhoop of Mataji Thali, is a cherished practice. This privilege is extended exclusively to daughters, daughters-in-law, or the daughters of daughters. In the absence of these individuals, the bhog should be graciously offered to a cow (not an ox). Males, however, are urged to refrain from partaking in the bhog, honoring the sanctity of this unique tradition.",
+    },
+  ],
+};
--- src/pages/poojavidhi.js
+++ src/pages/poojavidhi.js
@@ -2,13 +2,30 @@ import * as React from "react";
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import Layout from "../components/layout";
 import { PoojaVidhiConfig } from "../config/poojavidhi";
+import { PoojaConsiderations } from "../config/poojaconsiderations";
+
+const OrderedParagraphs = ({ items }) => {
+  return (
+    <List component="ol" sx={{ listStyle: "decimal", pl: 4 }}>
+      {items.map((item, index) => (
+        <ListItem sx={{ display: "list-item" }}>
+          <ListItemText
+            primary={item.title}
+            secondary={item.content}
+            align="justify"
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 const OrderedList = ({ items }) => {
   return (
     <List component="ol" sx={{ listStyle: "decimal", pl: 4 }}>
       {items.map((item, index) => (
         <ListItem key={index} sx={{ display: "list-item" }}>
-          <ListItemText primary={item.parent} />
+          <ListItemText primary={item.parent} align="justify" />
           {item.children && item.children.length > 0 && (
             <OrderedList items={item.children} />
           )}
@@ -23,6 +40,24 @@ const PoojaVidhi = () => {
     <Layout pageTitle="Pooja Vidhi">
       <Typography gutterBottom variant="h4" component="div" align="center">
         Pooja Vidhi
+      </Typography>
+      <br />
+      <Typography gutterBottom variant="h6" component="div" align="center">
+        <u>Considerations</u>
+      </Typography>
+      <Typography variant="body">
+        <OrderedParagraphs items={PoojaConsiderations.items} />
+      </Typography>
+      <Typography variant="body">
+        As you step into the precincts of our temple, may your journey be guided
+        by the profound spirituality and inclusive ethos that defines the
+        worship of Satimata Ji. Following is the complete Poojan Vidhi for our
+        Satimata Ji
+      </Typography>
+      <br />
+      <br />
+      <Typography gutterBottom variant="h6" component="div" align="center">
+        <u>Poojan Vidhi</u>
       </Typography>
       <Typography variant="body">
         <OrderedList items={PoojaVidhiConfig.items} />
