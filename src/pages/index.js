41f048f2da108da33b463be6baefe444bdb87b8b
Abhishek Patria <patriaabhishek@gmail.com>
2024-04-30 11:51:21 -0400

    Use image carousel and added supporting content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import Typography from "@mui/material/Typography";
+import ImageCarouselHomepage from "../components/ImageCarouselHomepage";
+import { Box } from "@mui/material";
 
 const Index = () => {
   return (
     <Layout pageTitle="Index">
-      <p1>To be done...</p1>
+      <Typography gutterBottom variant="h4" component="div" align="center">
+        Satimata of Patria-Didwania Samaj
+      </Typography>
+      <br/>
+      <Typography gutterBottom variant="h7" component="div" align="center">
+        Welcome to the pious digital abode of our Satimata.
+      </Typography>
+      <Box align="center">
+        <ImageCarouselHomepage />
+      </Box>
     </Layout>
   );
 };
 
-export const Head = () => <title>Index</title>;
+export const Head = () => <title>Satimata</title>;
 
 export default Index;
